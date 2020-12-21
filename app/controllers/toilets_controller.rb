f78db3d7927475122b9f74cfcd1a09acba5c1efd
takumi9332 <takumi9332@gmail.com>
2020-12-21 11:43:40 +0900

    ユーザーページ実装

--- app/controllers/toilets_controller.rb
+++ app/controllers/toilets_controller.rb
@@ -1,5 +1,7 @@
 class ToiletsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :set_toilet, only: [:show, :edit, :update, :destroy]
+
   def index
     @toilets = Toilet.all
     # @toilets = Toilet.includes(:user).order("rate DESC")
@@ -14,16 +16,41 @@ class ToiletsController < ApplicationController
     if @toilet.save
       redirect_to root_path
     else
-      render :new
+      redirect_to action: :new
     end
   end
 
   def show
-    @toilet = Toilet.find(params[:id])
+  end
+
+  def edit
+    unless current_user.id == @toilet.user_id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    if @toilet.update(toilet_params)
+      redirect_to action: :show
+    else
+      redirect_to action: :edit
+    end
+  end
+
+  def destroy
+    unless current_user.id == @toilet.user_id
+      redirect_to action: :index
+    end
+    @toilet.destroy
+    redirect_to root_path
   end
 
   private
   def toilet_params
     params.require(:toilet).permit(:image, :prefecture_id, :city, :address, :building, :sex_id, :type_id, :washlet_id, :clean_id, :info).merge(user_id: current_user.id)
+  end
+
+  def set_toilet
+    @toilet = Toilet.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'toilets#index'
   resources :toilets
+  resources :users, only: :show
 end
