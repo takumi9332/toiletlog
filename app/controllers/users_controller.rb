a8b77916e6bf0c62747b8e206def68efb2d62e88
takumi9332 <takumi9332@gmail.com>
2021-01-11 19:26:27 +0900

    ページネーションのバグ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,9 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @name = user.name
-    @toilets = user.toilets.order("created_at DESC").page(params[:page]).per(4)
+    @toilets = user.toilets.order("created_at DESC").page(params[:post_page]).per(4)
 
-    favorites = Favorite.where(user_id: current_user.id).pluck(:toilet_id)
-    @favorite_list = Toilet.order("created_at DESC").page(params[:page]).per(4).find(favorites)
+    @favorites = current_user.favorite_toilets.order("created_at DESC").page(params[:favorite_page]).per(4)
 
     respond_to do |format|
       format.html
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :toilets
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorite_toilets, through: :favorites, source: :toilet
 
   validates :name, presence: true, length: { maximum: 8 }
 end
