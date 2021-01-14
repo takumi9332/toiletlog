8794ad74a7702b7eae7a6ee3f54d2b52e09013d9
takumi9332 <takumi9332@gmail.com>
2021-01-14 11:25:09 +0900

    コメント削除機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,25 @@
 class CommentsController < ApplicationController
+  before_action :set_toilet
+  before_action :authenticate_user!
+
   def create
     comment = Comment.create(comment_params)
     redirect_to "/toilets/#{comment.toilet.id}"
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.user_id == current_user.id
+      @comment.destroy
+      redirect_to "/toilets/#{@comment.toilet.id}"
+    end
+  end
+
   private
+  def set_toilet
+    @toilet = Toilet.find(params[:toilet_id])
+  end
+
   def comment_params
     params.require(:comment).permit(:rate, :text).merge(user_id: current_user.id, toilet_id: params[:toilet_id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'toilets#index'
   get 'toilets/searchtoilet', to: 'toilets#search_toilet'
   resources :toilets do
-    resources :comments, only: :create
+    resources :comments, only: [:create, :destroy]
     resources :favorites, only: [:create, :destroy]
   end
   resources :users, only: :show do
